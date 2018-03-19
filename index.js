dd119aad3797d58a2f4f87b3cfe918cbf4adbf84
hawthorneben <hawthorneben@gmail.com>
2018-03-19 14:10:21 -0700

    Title header

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var ip = req.headers['x-forwarded-for'] ||
-     req.connection.remoteAddress ||
-     req.socket.remoteAddress ||
-     (req.connection.socket ? req.connection.socket.remoteAddress : null);
-  res.write(ip);
-  res.end();
+http.createServer(function (req, res)
+{
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var ip = req.headers['x-forwarded-for'] ||
+         req.connection.remoteAddress ||
+         req.socket.remoteAddress ||
+         (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    var title = "<title>Momentary Now</title>";
+    res.write(title);
+    res.write(ip);
+    res.end();
 }).listen(80);
