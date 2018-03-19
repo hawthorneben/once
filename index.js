c5d8b6a81e4959c11e206e58fc5de41168c6a345
hawthorneben <hawthorneben@gmail.com>
2018-03-19 14:47:41 -0700

    Connection to DB

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var http = require('http');
+const sqlite3 = require('sqlite3').verbose();
 http.createServer(function (req, res)
 {
     res.writeHead(200, {'Content-Type': 'text/html'});
+    let db = new sqlite3.Database('ip.db', sqlite3.OPEN_READWRITE, (err) =>
+    {
+        if (err)
+        {
+            console.error(err.message);
+        }
+        console.log('Connected to the IP database.');
+    });
     var ip = req.headers['x-forwarded-for'] ||
          req.connection.remoteAddress ||
          req.socket.remoteAddress ||
