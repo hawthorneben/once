33c0dc30396de8f76313edca84e6cb6782be058d
hawthorneben <hawthorneben@gmail.com>
2018-03-20 10:34:33 -0700

    Extensive commenting

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var sqlite3 = require('sqlite3');
 var fs = require('fs');
+
+// Create a server for each request
 http.createServer(function (req, res)
 {
 
@@ -12,26 +14,39 @@ http.createServer(function (req, res)
             console.error(err.message);
         }
     });
+
+    // Regex pattern will extract ip address from a string
     var ipMatch = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
+
+    // Various methods to obtain IP nearly guarantees that one will succeed
     var ipString = req.headers['x-forwarded-for'] ||
          req.connection.remoteAddress ||
          req.socket.remoteAddress ||
          (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+    // Extract IP address from the string containing it
     var ip = ipString.match(ipMatch);
+
+    // Our blacklisted flag is initially false
     var blacklisted = false;
 
+    // ensure that we obtained an IP address before proceeding
     if (ip)
     {
+        // SQL statement to check if client's ip is already in the database
         var checkString = "SELECT count(*) as num FROM LOOKUP WHERE VALUE = $ip";
+
+        // Serialize database to ensure synchronization
         db.serialize(() =>
         {
+            // Check if the count is greater than zero, if so the client is
+            // already blacklisted
             db.each(checkString, { $ip: ip[0] }, function(err, row)
             {
                 if (row.num > 0)
                 {
                     blacklisted = true;
-                }
-                console.log(blacklisted);
+                } // if
             }, function()
             {
                 if (blacklisted)
@@ -44,6 +59,7 @@ http.createServer(function (req, res)
                         }
                         else
                         {
+                            // Write the rejection page to the response
                             res.writeHead(200, {'Content-Type': 'text/html'});
                             res.end(data);
                         }
@@ -51,14 +67,16 @@ http.createServer(function (req, res)
                 } // if
                 else // user is not blacklisted yet
                 {
+                    // SQL statement ot insert ip addres into database
                     var statement = "INSERT INTO LOOKUP values($ip)";
 
+                    // Run the statement with the client's ip address
                     db.run(statement, { $ip : ip[0] }, function(err)
                     {
                         if (err)
                         {
                             console.error(err);
-                        }
+                        } // if
                     },
                     function()
                     { // Finished callback
@@ -67,16 +85,17 @@ http.createServer(function (req, res)
                             {
                                 res.statusCode = 500;
                                 res.end(`Error getting the file: ${err}.`);
-                            }
+                            } // if
                             else
                             {
+                                // Write the index file to the response
                                 res.writeHead(200, {'Content-Type': 'text/html'});
                                 res.end(data);
-                            }
-                        });
-                    });
-                }
-            });
-        });
-    }
+                            } // else
+                        }); // Read file callback
+                    }); // Insert statement callback
+                } // Not blacklisted block
+            }); // Blacklist check callback
+        }); // Database serialization callback
+    } // if
 }).listen(80);
