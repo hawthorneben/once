32c087fdef80ba855627a7f3c2c1a2672a214b03
hawthorneben <hawthorneben@gmail.com>
2018-03-19 19:47:37 -0700

    Regex match for IP

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 var http = require('http');
-const sqlite3 = require('sqlite3').verbose();
+var sqlite3 = require('sqlite3');
 http.createServer(function (req, res)
 {
     res.writeHead(200, {'Content-Type': 'text/html'});
+
+    // Open database
     let db = new sqlite3.Database('ip.db', sqlite3.OPEN_READWRITE, (err) =>
     {
         if (err)
         {
             console.error(err.message);
         }
-        console.log('Connected to the IP database.');
     });
-    var ip = req.headers['x-forwarded-for'] ||
+    var ipMatch = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
+    var ipString = req.headers['x-forwarded-for'] ||
          req.connection.remoteAddress ||
          req.socket.remoteAddress ||
          (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    var ip = ipString.match(ipMatch);
     var title = "<title>Momentary Now</title>";
     res.write(title);
-    res.write(ip);
+    if (ip)
+    {
+        res.write(ip);
+    }
     res.end();
+
+    // Close the database
+    db.close((err) =>
+    {
+        if (err)
+        {
+            console.error(err.message);
+        }
+    });
 }).listen(80);
